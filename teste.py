eba2b7e52af960e43d2367e66a5ee0c3d9066a23
Ana Giancoli <paulagiancoli@gmail.com>
2024-11-12 23:24:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -46,3 +46,28 @@ st.success("Você conseguiu!")
 st.error("Erro!")
 st.warning("Advertência")
 st.info("Esta é uma informação")
+
+st.write("**********************")
+#criando uma aplicação simples
+import streamlit as st
+#import no requirements.txt
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+
+st.write("Criando uma tabela!")
+#tabelas interativas
+st.write(df)
+#inserindo um selectbox
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     df['nomeServidor'])
+
+#LEMBRETE: O formato de print é diferente do fstring de Python
+st.write('Você selecionou: ', opcao)
+
+dfFiltrado = df.loc[df['nomeServidor'] == opcao]
+st.write(dfFiltrado)
