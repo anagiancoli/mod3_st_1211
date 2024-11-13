726696852d532b7f76a03667a7c3db382f2c8992
Ana Giancoli <paulagiancoli@gmail.com>
2024-11-12 23:22:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -17,3 +17,32 @@ color = st.select_slider(
 st.write("Satisfação: ", color)
 
 st.info(f"A satisfação foi de: {color}", icon=":material/thumb_up:" )
+
+st.write("******************")
+
+#criando elementos gráficos
+'''Informar como colher os dados através de variáveis'''
+x = st.checkbox('Sim')
+st.write(x)
+
+#fazer o mesmo com alguns dos comandos abaixo
+st.title(x)
+st.button('Clique')
+st.radio('Selecione seu gênero',['Masculino','Feminino'])
+st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
+st.slider('Selecione um número', 0,50)
+st.number_input('Selecione um número', 0,10)
+st.text_input('Endereço de e-mail')
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
+st.text_area('Descrição')
+st.file_uploader('Atualize uma foto')
+st.color_picker('Escolha sua cor favorita')
+
+#mensagens de status
+st.success("Você conseguiu!")
+st.error("Erro!")
+st.warning("Advertência")
+st.info("Esta é uma informação")
